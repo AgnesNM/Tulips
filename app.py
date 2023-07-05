1bc0725ee55e854da7d66f09fb7901b8f8c5b4f2
MundiaNderi <sarahngima77@gmail.com>
2023-07-05 15:57:17 +0300

    Main file

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, Response, render_template
 from send_sms import send_sms
 
 app = Flask(__name__)
 
-#TODO: create incoming messages route
+# home route
 
-#TODO: create delivery reports route.
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# create incoming messages route
+
+
+@app.route('/incoming-messages', methods=['POST'])
+def incoming_messages():
+    data = request.get_json(force=True)
+    print(f'Incoming message...\n ${data}')
+    return Response(status=200)
+
+# create delivery reports route.
+
+
+@app.route('/delivery-reports', methods=['POST'])
+def delivery_reports():
+    data = request.get_json(force=True)
+    print(f'Delivery report response...\n ${data}')
+    return Response(status=200)
+
 
 if __name__ == "__main__":
-    #TODO: Call send message function
-    
-    app.run(debug=True, port = os.environ.get("PORT"))
+    # TODO: Call send message function
+    send_sms()
+
+    app.run(debug=True, port=os.environ.get("PORT"))
